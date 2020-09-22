22ab154d740a060b05780c0ed407ec0db66a705a
danamur <59931318+danamur@users.noreply.github.com>
2020-09-22 19:45:39 -0300

    Conectando python a la base de datos
    primer ejercicio para conectar python a la base de datos de mysql

---
+++ BbddConPython/db.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+midb = mysql.connector.connect(
+    host='localhost',
+    user='chanchitoFeliz',
+    password='holamundo',
+    database ='prueba'
+)
+
+cursor = midb.cursor()
+
+cursor.execute('select * from Usuario')
+
+resultado = cursor.fetchall()
+
+print(resultado)
